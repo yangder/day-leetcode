9fffda0ce7efd5f92674e9fd2328925636d1e5fc
yangdw <yangdw@fshows.com>
2020-12-25 10:53:44 +0800

    leetcode_20有效括号

---
+++ src/main/java/com/yangde/leetcode/stock/LeetCode_20.java
@@ -0,0 +1,72 @@
+/**
+ * fshows.com
+ * Copyright (C) 2013-2020 All Rights Reserved.
+ */
+package com.yangde.leetcode.stock;
+
+import java.util.Stack;
+
+/**
+ * @author yangdw
+ * @version LeetCode_20.java, v 0.1 2020-12-24 20:56
+ * <p>
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ * 示例 1:
+ * <p>
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ * <p>
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ * <p>
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ * <p>
+ * 输入: "{[]}"
+ * 输出: true
+ */
+public class LeetCode_20 {
+
+    /**
+     * 有效括号 使用Stack类来处理
+     * stack.push()压栈,stack.pop()出栈并删除(栈顶数据),stack.peek()出栈但不删除(栈顶数据)
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || c != stack.pop()) return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid1(String s) {
+        int length = s.length() / 2;
+        for (int i = 0; i < length; i++) {
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        }
+        return s.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.err.println(isValid1("()"));
+    }
+}   
