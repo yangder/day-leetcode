e9c4aaad5b8be18f1c0756dbccaf56bed6982ab8
yangdw <yangdw@fshows.com>
2021-04-13 23:44:43 +0800

    cglib动态代理

--- src/main/java/com/yangde/designmode/proxy/ProxyTest.java
+++ src/main/java/com/yangde/designmode/proxy/ProxyTest.java
@@ -9,7 +9,7 @@ package com.yangde.designmode.proxy;
  * @version ProxyTest.java, v 0.1 2021-04-07 19:33
  */
 public class ProxyTest {
-
+    // https://www.jianshu.com/p/8ccdbe00ff06
     public static void main(String[] args) {
 
         OrderServiceProxy proxy = new OrderServiceProxy(new OrderServiceImpl());
